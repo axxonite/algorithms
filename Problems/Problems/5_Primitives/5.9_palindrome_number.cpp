0b20b8d06a91f62cc2363bf23e793a9e55556d6d
MOTHERSHIP\Dominic <dfilion@outlook.com>
2017-08-15 10:45:13 -0700

    Aded 5.9 redux.

---
+++ Problems/Problems/5_Primitives/5.9_palindrome_number.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2015 Elements of Programming Interviews. All rights reserved.
+
+#include "stdafx.h"
+
+#define TEST 0
+
+bool IsPalindromeNumber(int x) 
+{
+	return false;
+}
+
+#pragma region Test
+
+bool CheckAns(int x) {
+	auto s = to_string(x);
+	for (size_t i = 0, j = s.size() - 1; i < j; ++i, --j) {
+		if (s[i] != s[j]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void IsPalindromeNumberTest() 
+{
+#if TEST
+	default_random_engine gen((random_device())());
+	int x;
+	uniform_int_distribution<int> dis(-99999, 99999);
+	for (int times = 0; times < 1000; ++times) {
+		x = dis(gen);
+		assert(CheckAns(x) == IsPalindromeNumber(x));
+	}
+#endif
+}
+
+#pragma endregion 
