3a4d64003f85e91ca76a38799a0bcdffa26160d6
Dominic Filion <dfilion@hotmail.com>
2018-05-26 11:15:29 -0700

    Misc, changes.

--- EPI/Problems/7_Strings/7.7_phone_mnemonic.cpp
+++ EPI/Problems/7_Strings/7.7_phone_mnemonic.cpp
@@ -5,6 +5,8 @@
 
 #define TEST 0
 
+unordered_map<char, string> mappings = { { '2', "abc" },{ '3', "def" },{ '4', "ghi" },{ '5', "jkl" },{ '6', "mno" },{ '7', "pqrs" },{ '8', "tuv" },{ '9', "wxyz" } };
+
 vector<string> PhoneMnemonic( const string& n ) 
 {
 	vector<string> results;
--- EPI/Solutions/6_Arrays/6.3_bignumber_multiplication_solution.cpp
+++ EPI/Solutions/6_Arrays/6.3_bignumber_multiplication_solution.cpp
@@ -24,4 +24,33 @@ namespace Solutions
 		result.front() *= sign;
 		return result;
 	}
+
+	vector<int> Multiply2(vector<int> num1, vector<int> num2)
+	{
+		vector<int> result(num1.size() + num2.size(), 0);
+		int sign = ((num1.front() < 0) ^ (num2.front() < 0)) ? -1 : 1;
+		num1.front() = abs(num1.front()), num2.front() = abs(num2.front());
+		for (int i = 0; i < num1.size(); ++i)
+		{
+			for (int j = 0; j < num2.size(); ++j)
+			{
+				int index1 = num1.size() - 1 - i;
+				int index2 = num2.size() - 1 - j;
+				int dst = num1.size() + num2.size() - 1 - i - j;
+				result[dst] += num1[index1] * num2[index2];
+			}
+		}
+
+		for (int i = result.size() - 2; i >= 0; --i)
+			result[i] += result[i + 1] / 10, result[i + 1] %= 10;
+
+		int firstIndex = 0;
+		while (firstIndex < result.size() - 1 && result[firstIndex] == 0)
+			firstIndex++;
+
+		if (firstIndex != 0)
+			result = { result.begin() + firstIndex, result.end() };
+		result[0] *= sign;
+		return result;
+	}
 }
