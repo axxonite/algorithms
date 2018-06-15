65fe7e70c4682ead469918d8a2d2ad0fc13bff96
MOTHERSHIP\Dominic <dfilion@outlook.com>
2018-06-14 20:28:04 -0700

    Updated 2-sum solution.

--- EPI/Solutions/18_GreedyAlgorithms/2_sum_unsorted_solution.cpp
+++ EPI/Solutions/18_GreedyAlgorithms/2_sum_unsorted_solution.cpp
@@ -4,13 +4,13 @@ namespace Solutions
 {
 	inline bool HasTwoSum(const vector<int>& A, int t)
 	{
-		unordered_set<int> values(A.begin(), A.end()); // Add all values in the vector to a set so we can query for the existence of specific values in the vector.
+		unordered_multiset<int> values(A.begin(), A.end()); // Add all values in the set to a set so we can query for the existence of specific values in the set.
 		for (int i = 0; i < A.size(); ++i)
 		{
 			// Long for the value that would add up to t, given the first value A[i].
 			// Watch for overflow.
 			long long difference = long long(t) - long long(A[i]);
-			if ( difference >= numeric_limits<int>::min() && difference <= numeric_limits<int>::max() && values.count(difference))
+			if ( difference >= numeric_limits<int>::min() && difference <= numeric_limits<int>::max() && values.count(difference) > difference == A[i] ? 1 : 0)
 				return true;
 		}
 		return false;
