37c57acc187d9b6a63e04e6e582d77571a9b2013
Dominic Filion Guay <dominic.filion-guay@oculus.com>
2019-05-29 15:01:14 -0700

    Fixed up MST Kruskal.

--- EPI/Problems/19_Graphs/MSTKruskal.cpp
+++ EPI/Problems/19_Graphs/MSTKruskal.cpp
@@ -1,8 +1,9 @@
 #include "stdafx.h"
 #include "..\..\Solutions\Data Structures\DisjointSet_solution.h"
 #include "Problems/Data Structures/MSTEdge.h"
+#include "GraphVertex.h"
 
-#define TEST 0
+// #define TEST
 
 struct MSTKruskalGraphVertex;
 
@@ -10,7 +11,7 @@ typedef MSTEdge<MSTKruskalGraphVertex> MSTKruskalEdge;
 
 struct MSTKruskalGraphVertex : Solutions::SetElem
 {
-	char data;
+	char data{};
 	vector<shared_ptr<MSTKruskalEdge>> edges;
 };
 
@@ -25,7 +26,7 @@ struct HashEdge
 unordered_set<shared_ptr<MSTKruskalEdge>, HashEdge> MstKruskal(vector<shared_ptr<MSTKruskalGraphVertex>> g)
 {
 	unordered_set<shared_ptr<MSTKruskalEdge>, HashEdge> result;
-	return result;
+  return result;
 }
 
 #pragma region Test
@@ -40,7 +41,7 @@ shared_ptr<MSTKruskalEdge> AddEdge(MSTKruskalEdge edge)
 
 void MstKruskalTest()
 {
-#if TEST
+#ifdef TEST
 	auto a = make_shared<MSTKruskalGraphVertex>();
 	auto b = make_shared<MSTKruskalGraphVertex>();
 	auto c = make_shared<MSTKruskalGraphVertex>();
@@ -60,30 +61,26 @@ void MstKruskalTest()
 	h->data = 'h';
 	i->data = 'i';
 
-	auto goldenResult = unordered_set<shared_ptr<Edge>, HashEdge>();
-	goldenResult.emplace(AddEdge(Edge{ a.get(), b.get(), 4 }));
-	goldenResult.emplace(AddEdge(Edge{ c.get(), d.get(), 7 }));
-	goldenResult.emplace(AddEdge(Edge{ c.get(), i.get(), 2 }));
-	goldenResult.emplace(AddEdge(Edge{ c.get(), f.get(), 4 }));
-	goldenResult.emplace(AddEdge(Edge{ d.get(), e.get(), 9 }));
-	goldenResult.emplace(AddEdge(Edge{ g.get(), h.get(), 1 }));
-	goldenResult.emplace(AddEdge(Edge{ f.get(), g.get(), 2 }));
-	goldenResult.emplace(AddEdge(Edge{ a.get(), h.get(), 8 }));
-	AddEdge(Edge{ b.get(), c.get(), 8 });
-	AddEdge(Edge{ b.get(), h.get(), 11 });
-	AddEdge(Edge{ d.get(), f.get(), 14 });
-	AddEdge(Edge{ e.get(), f.get(), 10 });
-	AddEdge(Edge{ g.get(), i.get(), 6 });
-	AddEdge(Edge{ h.get(), i.get(), 7 });
+	auto goldenResult = unordered_set<shared_ptr<MSTKruskalEdge>, HashEdge>();
+	goldenResult.emplace(AddEdge(MSTKruskalEdge{ a.get(), b.get(), 4 }));
+	goldenResult.emplace(AddEdge(MSTKruskalEdge{ c.get(), d.get(), 7 }));
+	goldenResult.emplace(AddEdge(MSTKruskalEdge{ c.get(), i.get(), 2 }));
+	goldenResult.emplace(AddEdge(MSTKruskalEdge{ c.get(), f.get(), 4 }));
+	goldenResult.emplace(AddEdge(MSTKruskalEdge{ d.get(), e.get(), 9 }));
+	goldenResult.emplace(AddEdge(MSTKruskalEdge{ g.get(), h.get(), 1 }));
+	goldenResult.emplace(AddEdge(MSTKruskalEdge{ f.get(), g.get(), 2 }));
+	goldenResult.emplace(AddEdge(MSTKruskalEdge{ a.get(), h.get(), 8 }));
+	AddEdge(MSTKruskalEdge{ b.get(), c.get(), 8 });
+	AddEdge(MSTKruskalEdge{ b.get(), h.get(), 11 });
+	AddEdge(MSTKruskalEdge{ d.get(), f.get(), 14 });
+	AddEdge(MSTKruskalEdge{ e.get(), f.get(), 10 });
+	AddEdge(MSTKruskalEdge{ g.get(), i.get(), 6 });
+	AddEdge(MSTKruskalEdge{ h.get(), i.get(), 7 });
 
-	auto result = MstKruskal(vector<shared_ptr<MSTKruskalGraphVertex>>({ a, b, c, d, e, f, g, h, i }));
-	assert(result.size() == 8);
-	auto weight = 0;
-	for (auto edge : result)
-		weight += edge->weight;
-	assert(weight == 37);
-	for (auto edge : goldenResult)
-		assert(result.count(edge));
+  auto result = MstKruskal(vector<shared_ptr<MSTKruskalGraphVertex>>({ a, b, c, d, e, f, g, h, i }));
+  assert(result.size() == 8);
+  const auto weight = accumulate(result.begin(), result.end(), 0, [](int value, const shared_ptr<MSTKruskalEdge>& edge) { return value + edge->weight; });
+  assert(weight == 37);
 #endif
 }
 
