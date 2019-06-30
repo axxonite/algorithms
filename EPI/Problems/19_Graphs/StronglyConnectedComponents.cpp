91f8d427874b664623c6d8664874d93f6acc7293
MOTHERSHIP\Dominic <dfilion@outlook.com>
2019-06-29 18:43:36 -0700

    Misc. fixups.

--- EPI/Problems/19_Graphs/StronglyConnectedComponents.cpp
+++ EPI/Problems/19_Graphs/StronglyConnectedComponents.cpp
@@ -6,7 +6,7 @@
 vector<unordered_set<GraphVertex*>> StronglyConnectedComponents(vector<shared_ptr<GraphVertex>> G)
 {
 	vector<unordered_set<GraphVertex*>> result;
-  return result;
+	return result;
 }
 
 #pragma region Test
@@ -18,20 +18,20 @@ void StronglyConnectedComponentsTest()
 	for (int i = 0; i < 8; ++i)
 		G.emplace_back(make_shared<GraphVertex>(GraphVertex()));
 
-	G[0]->edges.emplace_back(Edge{G[1].get(), 1});
-	G[1]->edges.emplace_back(Edge{G[2].get(), 1});
-	G[1]->edges.emplace_back(Edge{G[4].get(), 1});
-	G[1]->edges.emplace_back(Edge{G[5].get(), 1});
-	G[2]->edges.emplace_back(Edge{G[3].get(), 1});
-	G[2]->edges.emplace_back(Edge{G[6].get(), 1});
-	G[3]->edges.emplace_back(Edge{G[2].get(), 1});
-	G[3]->edges.emplace_back(Edge{G[7].get(), 1});
-	G[4]->edges.emplace_back(Edge{G[0].get(), 1});
-	G[4]->edges.emplace_back(Edge{G[5].get(), 1});
-	G[5]->edges.emplace_back(Edge{G[6].get(), 1});
-	G[6]->edges.emplace_back(Edge{G[5].get(), 1});
-	G[6]->edges.emplace_back(Edge{G[7].get(), 1});
-	G[7]->edges.emplace_back(Edge{G[7].get(), 1});
+	G[0]->edges.emplace_back(Edge{ G[1].get(), 1 });
+	G[1]->edges.emplace_back(Edge{ G[2].get(), 1 });
+	G[1]->edges.emplace_back(Edge{ G[4].get(), 1 });
+	G[1]->edges.emplace_back(Edge{ G[5].get(), 1 });
+	G[2]->edges.emplace_back(Edge{ G[3].get(), 1 });
+	G[2]->edges.emplace_back(Edge{ G[6].get(), 1 });
+	G[3]->edges.emplace_back(Edge{ G[2].get(), 1 });
+	G[3]->edges.emplace_back(Edge{ G[7].get(), 1 });
+	G[4]->edges.emplace_back(Edge{ G[0].get(), 1 });
+	G[4]->edges.emplace_back(Edge{ G[5].get(), 1 });
+	G[5]->edges.emplace_back(Edge{ G[6].get(), 1 });
+	G[6]->edges.emplace_back(Edge{ G[5].get(), 1 });
+	G[6]->edges.emplace_back(Edge{ G[7].get(), 1 });
+	G[7]->edges.emplace_back(Edge{ G[7].get(), 1 });
 	auto result = StronglyConnectedComponents(G);
 	assert(result[0] == unordered_set<GraphVertex*>({ G[0].get(), G[1].get(), G[4].get() }));
 	assert(result[1] == unordered_set<GraphVertex*>({ G[2].get(), G[3].get() }));
