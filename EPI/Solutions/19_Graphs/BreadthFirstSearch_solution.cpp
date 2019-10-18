fd6964a1421873e2f940978c40714226a72b6632
Axxon <dominic.filion-guay@oculus.com>
2019-10-18 12:28:33 -0400

    Fixed up BFS solution.

--- EPI/Solutions/19_Graphs/BreadthFirstSearch_solution.cpp
+++ EPI/Solutions/19_Graphs/BreadthFirstSearch_solution.cpp
@@ -3,34 +3,34 @@
 
 namespace Solutions
 {
-#include "stdafx.h"
+	void BFS(GraphVertex* v)
+	{
+		queue<GraphVertex*> q;
+		v->dist = 0;
+		q.emplace(v);
+		v->stage = 1;
+		while (!q.empty())
+		{
+			auto cur = q.front();
+			q.pop();
+			cur->stage = 2;
+			for (auto e : cur->edges)
+			if (e.dst->stage == 0)
+			{
+				e.dst->pred = v;
+				e.dst->dist = v->dist + 1;
+				e.dst->stage = 1;
+				q.emplace(e.dst);
+			}
+		}
+	}
 
-  void BFS(GraphVertex* v)
-  {
-    queue<GraphVertex*> q;
-    v->dist = 0;
-    q.emplace(v);
-    while (!q.empty())
-    {
-      auto cur = q.front();
-      q.pop();
-      cur->stage = 1;
-      for (auto e : cur->edges)
-        if (e.dst->stage == 0)
-        {
-          e.dst->pred = v;
-          e.dst->dist = v->dist + 1;
-          e.dst->stage = 1;
-          q.emplace(e.dst);
-        }
-      cur->stage = 2;
-    }
-  }
-
-  void BFS(vector<GraphVertex*> g)
-  {
-    for (auto v : g)
-      if (v->stage == 0)
-        BFS(v);
-  }
+	void BFS(vector<GraphVertex*> g)
+	{
+		for (auto v : g)
+		{
+			if (v->stage == 0)
+				BFS(v);
+		}
+	}
 }
